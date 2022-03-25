faa5a813cd3edf1b3c1fd8e8793404faa3c585ac
dilana_sancho <dilana.sancho@factork-software.com>
2022-03-25 15:35:03 -0600

    Update test.py
    agregado un else porque la regla dice que necesita un else.
    hay que cambiar la regla.

--- test.py
+++ test.py
@@ -1,26 +1,28 @@
 #Calcular factorial
 def calcularFac(num):
-    num_aux = 0
+    numaux = 0
     if (num < 1):
-        num_aux = 1
+        numaux = 1
     else:
-        num_aux = num * (calcularFac(num-1))
-    return num_aux
+        numaux = num * (calcularFac(num-1))
+    return numaux
 
 ventas = [100,200,300,400,500]
 
 #promedio de elementos
-def promedio(cualquier_arreglo):
-    tam = len(cualquier_arreglo)
+def promedio(cualquierarreglo):
+    tam = len(cualquierarreglo)
     resultado=0
     #ciclo para recorrer arreglo
     cont = 0
     sumatoria = 0
     while (cont <= tam-1):
-        sumatoria = sumatoria + cualquier_arreglo[cont]
-        cont += 1
+        sumatoria = sumatoria + cualquierarreglo[cont]
+        cont = cont + 1
     if tam > 0:
         resultado = sumatoria / tam
+    else:
+        print("Soy genial")
     return resultado
 
 def calcularpromedioventas():
