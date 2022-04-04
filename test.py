a927d0cceb6233da7bc817373ac0adc13ed539ab
dilana_sancho <dilana.sancho@factork-software.com>
2022-04-03 18:17:43 -0600

    Update test.py
    Los comentarios que hay que hacerle para la revision de la progra con el codigo que mando el profe

--- test.py
+++ test.py
@@ -1,35 +1,48 @@
+"""
+	Cdigo de prueba para
+	# Anlisis Sintctico #
+
+
+quitamos los caracteres especiales de los string como tildes
+
+"""
+
 #Calcular factorial
-
-def calcularFac (num):
-
-    numaux = 0
+def calcularFac(num):
+    num_aux = 0
     if (num < 1):
-        numaux = 1
+        num_aux = -1
     else:
-        numaux = num * (calcularFac(num-1))
-    return numaux
-ventas = [100,200,300,400,500]
+        #el numero uno tine que estar entre parenticis para que divida las expreciones
+        num_aux = num * (calcularFac(num-(-1)))
+    return num_aux
+
+ventas = [100,200,300,400,500,-3.14]
+
+Caracter = '%'
 
 #promedio de elementos
-def promedio(cualquierarreglo):
-    tam = len(cualquierarreglo)
+def promedio(cualquier_arreglo):
+    tam = len(cualquier_arreglo)
     resultado=0
     #ciclo para recorrer arreglo
     cont = 0
     sumatoria = 0
     while (cont <= tam-1):
-        sumatoria = sumatoria + cualquierarreglo[cont]
+        sumatoria = sumatoria + cualquier_arreglo[cont]
         cont = cont + 1
     if tam > 0:
         resultado = sumatoria / tam
-    else:
-        print("Soy genial")
     return resultado
 
 def calcularpromedioventas():
-    prom = promedio(ventas)
+    prom = promedio(len(ventas))
     print("El promedio de las ventas es: ")
+    #Para que no de error aqui el print tiene que estar entre comillas dobles
+    # porque la regla dice que la exprecion puede ser un string
+    # pero el string  debe estar entre comillas dobles
+    print("\n")
     print(prom)
 
-print("Calculo del Factorial: " + calcularFac(6))
+print("Calculo del Factorial:%$ " + calcularFac(6))
 calcularpromedioventas()
